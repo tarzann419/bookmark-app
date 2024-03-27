324b549d99b6bd26bd021440bf013a39ed3bfb95
dantheman <danogbo0@gmail.com>
2024-03-27 16:28:11 +0100

    sign in module complete

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,10 +10,10 @@ export class AuthController {
 
     @Post('signup')
     signup(@Body() dto: AuthDto) {
-        console.log({
-            dto,
-        })
-        return this.authService.signup();
+        // console.log({
+        //     dto,
+        // })
+        return this.authService.signup(dto);
     }
 
     @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,47 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService){}
     
-    signup(){
-        return 'this is the sign up page';
+    async signup(dto: AuthDto){
+        // generate password
+        const hash = await argon.hash(dto.password);
+
+        // save new user in db
+
+        try{
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+                // show only the following fiedls
+                // select: {
+                //     id: true,
+                //     email: true,
+                //     createdAt: true,
+                // }
+            });
+            // or delete only a specific field
+            delete user.hash;
+    
+            return user;
+        } catch(error){
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code == 'P2002') {
+                    throw new ForbiddenException('Email already taken')
+                }
+            }
+            throw error;
+        }
+        // return saved user credentials
+        
     }
 
     signin() {}
